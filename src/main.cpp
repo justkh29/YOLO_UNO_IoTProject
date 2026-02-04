a07768202e8f7960d58e43ad3831b7828e375aee
Hung Ta <khanghung711@gmail.com>
2026-02-04 10:05:10 +0700

    Project init

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <global.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // Serial.println("Hello Custom Board");
+  // delay(1000);
+}

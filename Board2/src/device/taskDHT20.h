2687177c697f60c682be4c345562875f8c7de948
Hung Ta <khanghung711@gmail.com>
2026-05-23 12:08:08 +0700

    remove unused files

--- Board2/src/device/taskDHT20.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_TASKDHT11_H_
-#define INC_TASKDHT11_H_
-
-#include "global.h"
-#define DHTPin 16
-extern void taskDHT20(void *pvParameters);
-extern void taskBlinkLED(void *pvParameters);
-
-#endif 
--- Board2/src/device/taskLCD.h
+++
@@ -1,7 +0,0 @@
-#ifndef TASKLCD_H
-#define TASKLCD_H
-
-#include <global.h>
-
-void taskLCD(void *pvParameters);
-#endif
